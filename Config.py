df0bf9b841a19d9fd15ab042e163fd0d41dc7ead
scott francis <scott.russell.francis@gmail.com>
2020-09-15 11:53:02 -0700

    add config singleton

---
+++ Config.py
@@ -0,0 +1,12 @@
+# Global configuration dict object for the app
+#
+#   can be used as state as well
+#
+
+# these are the defaults, they can be overwritten
+state = {
+    'file': 's3://connected-vehicle-datasource/100.csv',
+    'topic_base': "vt",
+    'at_end': 'repeat',
+    'local_dir': '/tmp'
+}
